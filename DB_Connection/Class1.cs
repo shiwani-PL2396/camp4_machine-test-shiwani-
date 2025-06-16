aae0ac56796b211d1b81d2a7786a741609832543
shiwani-PL2396 <suman301shk@gmail.com>
2025-06-16 14:33:01 +0530

    Add files via upload

---
+++ DB_Connection/Class1.cs
@@ -0,0 +1,41 @@
+﻿namespace DB_Connection
+{
+    using Microsoft.Data.SqlClient;
+    using System.Configuration;
+    namespace ClassLibraryDatabaseConnect
+    {
+        public class DatabaseConnectionManager
+        {
+            //Each project is having App.config -- feth connection string from there
+
+            //Estabilish the connection and return
+            //Sql Connection
+            public static SqlConnection OpenConnection(string _connectionString)
+            {
+                SqlConnection connection = null;
+                try
+                {
+                    if (_connectionString != null || Convert.ToString(connection.State) == "Closed")
+                    {
+                        connection = new SqlConnection(_connectionString);
+                        connection.Open();
+                    }
+                    return connection;
+                }
+                catch (SqlException sqlEx)
+                {
+                    //Log the SQL exception
+                    Console.WriteLine("SQL Error: " + sqlEx.Message);
+                    return null;
+                }
+
+                catch (Exception ex)
+                {
+                    //Log the exception
+                    Console.WriteLine("Error: " + ex.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
